dc86f8b2fd9ca00c3a432d0ab8adcc955707628a
spo9183 <spo9183@gmail.com>
2020-08-01 21:56:25 +0000

    Lab 6

--- main.js
+++ main.js
@@ -84,22 +84,6 @@ class ServiceNowAdapter extends EventEmitter {
   }
 
   /**
-   * @memberof ServiceNowAdapter
-   * @method healthcheck
-   * @summary Check ServiceNow Health
-   * @description Verifies external system is available and healthy.
-   *   Calls method emitOnline if external system is available.
-   *
-   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
-   *   that handles the response.
-   */
-  healthcheck(callback) {
-    // We will build this method in a later lab. For now, it will emulate
-    // a healthy integration by emmitting ONLINE.
-    this.emitOnline();
-  }
-
-  /**
  * @memberof ServiceNowAdapter
  * @method healthcheck
  * @summary Check ServiceNow Health
